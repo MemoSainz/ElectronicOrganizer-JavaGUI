507b8fc29ca7998ba5a6e99cf27d540477e6a54b
MemoSainz <sainzmemo_21@proton.me>
2024-08-04 06:15:06 -0700

    first commit

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,21 @@
+
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+/**
+ *
+ * @author Memo Sainz
+ */
+public class AgendaElectronica {
+
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+    
+}
